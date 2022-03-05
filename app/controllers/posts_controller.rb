70bf9328d322a38bed83986921f0445bd7621a0a
taiga07 <taigatani@me.com>
2022-03-05 02:46:55 +0000

    [finish]post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,15 +20,27 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
 
