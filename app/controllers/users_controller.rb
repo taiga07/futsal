1ff5e2de63084410ce91c805fd69f6b36ac05844
taiga07 <taigatani@me.com>
2022-03-05 06:23:57 +0000

    [finish]user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  
+
   # ユーザー新規登録画面
   def new
     @user = User.new
   end
-  
+
   # 新規登録
   def create
     @user = User.new(user_params)
@@ -14,22 +14,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: :desc)
   end
-  
+
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
-  
-  
-  
+
+
+
   private
-  
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :introduction, :password, :password_confirmation)
   end
-  
+
 end
